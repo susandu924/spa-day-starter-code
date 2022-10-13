2a55bb26e73f7a690c4f1774deabeebc6f182d85
Susan <susandu924@gmail.com>
2022-10-12 19:58:43 -0500

    spa-menu-studio-part-2-first-commit

---
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -0,0 +1,20 @@
+package org.launchcode.spaday.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+@GetMapping("/user/add")
+public String displayAddUserForm() {
+    return "user/add";
+}
+
+
+
+}
---
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -0,0 +1,44 @@
+package org.launchcode.spaday.models;
+
+public class User {
+
+    private static String username;
+    private static String email;
+    private static String password;
+
+    private int id;
+
+    private int nextId = 1;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.id = nextId;
+        nextId++;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public static void setUsername(String username) {
+        User.username = username;
+    }
+
+    public static String getEmail() {
+        return email;
+    }
+
+    public static void setEmail(String email) {
+        User.email = email;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+
+    public static void setPassword(String password) {
+        User.password = password;
+    }
+}
