b80983df279f9634e047f5550523eccecb9147b5
Susan <susandu924@gmail.com>
2022-10-18 23:02:24 -0500

    spa-menu-studio-user-signup-pt2

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -9,11 +9,30 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("user")
 public class UserController {
 
+//  TO DO 2:  Using the Model to Render the Form¶
+//In the UserController, modify the add method that displays the form so that it passes in an empty User object with:
+//
+//model.addAttribute(new User());
+//This object will be accessible in the template, by name, as user.
+
+//    Now that you’re passing in an empty User object, you may notice some redundant code in your processAddUserForm controller.
+//    Remove the model attribute additions and update the user/add template to make use of the model fields (eg. user.username).
+
+
+
     @GetMapping("/add")
     public String displayAddUserForm() {
         return "user/add";
     }
 
+
+//   TO DO 4: Now that you have your form set up, go back to UserController and add validation on form submission
+//    by adding the @Valid annotation to the User parameter that is bound, along with an additional parameter: Errors errors.
+//
+//Warning
+//Remember, you must put this parameter directly after the User parameter in the method definition for it to work properly.
+//
+//Within the processAddUserForm handler, check for errors configured by the validation annotation using errors.hasErrors(). If this returns true, return the user to the form
     @PostMapping
     public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
         model.addAttribute("user", user);
--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,6 +1,22 @@
 package org.launchcode.spaday.models;
 
 public class User {
+
+//    TO DO 1:
+//    Add Validation Annotations¶
+//Navigate to the User model class. Add validation annotations to ensure these conditions are satisfied:
+//
+//Username, password, and verify are required (they can’t be empty)
+//Username is between 5 and 15 characters
+//Email is optional
+//If provided, the email has the format of a valid email address.
+//The password is at least 6 characters lon
+
+//    TO DO 5: As we mentioned above, we are not able to use Spring’s validation machinery to validate that the two password fields match given the setup we have here. Checking errors.hasErrors() will only tell us if there are errors in other form data fields.
+//
+//Last studio, we added some validation checks to make sure the password fields match. Now we have two validation sections: one for the annotation-configured validation (which checks errors.hasErrors()), and one that checks that the password fields match.
+// Make sure they work in-sync with each other to properly return to the form if any of the validation conditions fail.
+
     private String username;
     private String email;
     private String password;
